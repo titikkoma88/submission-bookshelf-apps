d0cf90e003848bd0c4a1ede830a7f61342d0ac0d
titikkoma88 <jie.koma88@gmail.com>
2022-02-16 11:11:15 +0700

    Save Book Data in localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,31 @@
 const books = [];
 const RENDER_EVENT = "render-book";
+const SAVED_EVENT = "saved-book";
+const STORAGE_KEY = "BOOKS_DATA";
 
-document.addEventListener("DOMContentLoaded", function () {
- 
-    const submitForm = document.getElementById("inputBook");
-    const inputSearchBook = document.getElementById("searchBook");
-    const inputBookIsComplete = document.getElementById("inputBookIsComplete");
- 
-    submitForm.addEventListener("submit", function (event) {
-        event.preventDefault();
-        addBook();
-    });
+function isStorageExist() {
+    if(typeof(Storage) === undefined){
+        alert("Browser kamu tidak mendukung local storage");
+        return false
+    }
+    return true;
+}
 
-    inputSearchBook.addEventListener("keyup", function (event) {
-        event.preventDefault();
-        searchBook();
-      });
-    
-      inputSearchBook.addEventListener("submit", function (event) {
-        event.preventDefault();
-        searchBook();
-      });
-    
-      inputBookIsComplete.addEventListener("input", function (event) {
-        event.preventDefault();
-        checkButton();
-      });
-    
-});
+function generateId() {
+    return +new Date();
+}
+ 
+function generateBookObject(id, title, author, year, purchasing, timestamp, isCompleted) {
+    return {
+        id,
+        title,
+        author,
+        year,
+        purchasing,
+        timestamp,
+        isCompleted
+    }
+}
 
 function addBook() {
     const textTitle = document.getElementById("inputBookTitle").value;
@@ -44,6 +42,7 @@ function addBook() {
    
     clearForm();
     document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
 }
 
 function checkButton() {
@@ -52,22 +51,6 @@ function checkButton() {
         span.innerText = "Selesai dibaca";
     } else {
         span.innerText = "Belum selesai dibaca";
-    }
-}
-
-function generateId() {
-    return +new Date();
-}
- 
-function generateBookObject(id, title, author, year, purchasing, timestamp, isCompleted) {
-    return {
-        id,
-        title,
-        author,
-        year,
-        purchasing,
-        timestamp,
-        isCompleted
     }
 }
 
@@ -174,6 +157,7 @@ function makeListBook(bookObject) {
   
     bookTarget.isCompleted = true;
     document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
 }
 
 function removeBook(bookId) {
@@ -186,6 +170,7 @@ function removeBook(bookId) {
    
         document.dispatchEvent(new Event(RENDER_EVENT));
         alert("Buku berhasil dihapus");
+        saveData();
     } else {
         alert("Buku gagal dihapus");
     }
@@ -198,6 +183,7 @@ function undoBookFromCompleted(bookId){
    
     bookTarget.isCompleted = false;
     document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
 }
 
 function clearForm() {
@@ -240,9 +226,61 @@ function searchBook() {
     }
 }
 
+function saveData() {
+    if(isStorageExist()){
+        const parsed = JSON.stringify(books);
+        localStorage.setItem(STORAGE_KEY, parsed);
+        document.dispatchEvent(new Event(SAVED_EVENT));
+        alert("Data akan disalin atau memperbaharui web storage?");
+    }
+  }
+
+function loadDataFromStorage() {
+    const serializedData = localStorage.getItem(STORAGE_KEY);
+   
+    let data = JSON.parse(serializedData);
+   
+    if(data !== null){
+        for(book of data){
+            books.push(book);
+        }
+    }
+   
+    document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+ 
+    const submitForm = document.getElementById("inputBook");
+    const inputSearchBook = document.getElementById("searchBook");
+    const inputBookIsComplete = document.getElementById("inputBookIsComplete");
+ 
+    submitForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+        addBook();
+    });
+
+    inputSearchBook.addEventListener("keyup", function (event) {
+        event.preventDefault();
+        searchBook();
+    });
+    
+    inputSearchBook.addEventListener("submit", function (event) {
+        event.preventDefault();
+        searchBook();
+    });
+    
+    inputBookIsComplete.addEventListener("input", function (event) {
+        event.preventDefault();
+        checkButton();
+    });
+    
+    if(isStorageExist()){
+        loadDataFromStorage();
+    }
+});
 
 document.addEventListener(RENDER_EVENT, function () {
-    console.log(books);
     const uncompletedBookList = document.getElementById("incompleteBookshelfList");
     uncompletedBookList.innerHTML = "";
 
@@ -259,3 +297,7 @@ document.addEventListener(RENDER_EVENT, function () {
         
     }
  });
+
+ document.addEventListener(SAVED_EVENT, function() {
+    console.log(localStorage.getItem(STORAGE_KEY));
+  });
